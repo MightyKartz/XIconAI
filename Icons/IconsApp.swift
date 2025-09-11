1f75e9729b90ee54254c14195626cd4d1767df74
MightyKartz <kartz.cn@gmail.com>
2025-09-11 15:27:54 +0800

    Initial Commit

---
+++ Icons/IconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconsApp.swift
+//  Icons
+//
+//  Created by Kartz on 2025/9/11.
+//
+
+import SwiftUI
+
+@main
+struct IconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IconsTests/IconsTests.swift
@@ -0,0 +1,17 @@
+//
+//  IconsTests.swift
+//  IconsTests
+//
+//  Created by Kartz on 2025/9/11.
+//
+
+import Testing
+@testable import Icons
+
+struct IconsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
